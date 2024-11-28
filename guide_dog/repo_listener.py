1d7892a0b62e7ee4bda805e5c35095665da01ae2
AnneRei <anne.reichert97@gmail.com>
2024-11-28 13:46:15 +0100

    rebase while pulling

--- guide_dog/repo_listener.py
+++ guide_dog/repo_listener.py
@@ -74,7 +74,7 @@ class GuideDogListener:
     def pull_changes(self):
         repo = Repo(self.LOCAL_REPO_PATH)
         origin = repo.remotes.origin
-        origin.pull(self.BRANCH_NAME)
+        origin.pull(self.BRANCH_NAME, "--rebase")
 
     # Commit and push any new files
     def commit_and_push(self, commit_file):
